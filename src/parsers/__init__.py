f6fb7cdd5334faa1444347c3e82445743123296c
CineGiraffe <122433569+CineGiraffe@users.noreply.github.com>
2024-07-03 22:09:43 +0300

    1

--- src/parsers/__init__.py
+++ src/parsers/__init__.py
@@ -1,14 +1,14 @@
 from src.parsers.parser import Parser
 
-from src.parsers.universities.BMSTU import BmstuParser
-from src.parsers.universities.ITMO import ItmoParser
-from src.parsers.universities.MAI import MaiParser
-from src.parsers.universities.MIET import MietParser
-from src.parsers.universities.MIPT import MiptParser
-from src.parsers.universities.MIREA import MireaParser
-from src.parsers.universities.MPEI import MpeiParser
-from src.parsers.universities.MPOLITECH import MpolitechParser
-from src.parsers.universities.MTUCI import MtuciParser
-from src.parsers.universities.SPBSU import SpbsuParser
+# from src.parsers.universities.BMSTU import BmstuParser
+# from src.parsers.universities.ITMO import ItmoParser
+# from src.parsers.universities.MAI import MaiParser
+# from src.parsers.universities.MIET import MietParser
+# from src.parsers.universities.MIPT import MiptParser
+# from src.parsers.universities.MIREA import MireaParser
+# from src.parsers.universities.MPEI import MpeiParser
+# from src.parsers.universities.MPOLITECH import MpolitechParser
+# from src.parsers.universities.MTUCI import MtuciParser
+# from src.parsers.universities.SPBSU import SpbsuParser
 from src.parsers.universities.VSE import VseParser
 from src.parsers.universities.RSMU import RsmuParser
