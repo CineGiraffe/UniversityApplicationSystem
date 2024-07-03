7b7e2b600950e50458549fe97277b068381a559e
CineGiraffe <122433569+CineGiraffe@users.noreply.github.com>
2024-07-03 21:24:37 +0300

    1

---
+++ src/parsers/universities/RSMU.py
@@ -0,0 +1,31 @@
+from src.core import StudentId, University
+from src.parsers.parser import CsvParser, FileExtension, HeadersMapping
+
+from re import match
+
+
+class RsmuParser(CsvParser):
+
+    def for_university(self) -> University:
+        return University.RSMU
+
+    def _headers_mapping(self, file_extension: FileExtension) -> HeadersMapping:
+        return HeadersMapping('Уникальный код', 'Общая сумма баллов', 'Оригинал в вузе')
+
+    def _parse_student_id(self, raw_id: str) -> StudentId:
+        if match('[1-9][0-9]{2}-[0-9]{3}-[0-9]{3} [0-9]{2}', raw_id):
+            return StudentId(raw_id)
+        else:
+            raise Exception("found incompatible id", raw_id)
+
+    def _parse_dormitory_requirement(self, raw_value: str) -> bool:
+        # dummy details in file (just auto set True)
+        return True
+
+    def _parse_agreement_submission(self, raw_value: str) -> bool:
+        if raw_value == 'Yes':
+            return True
+        elif raw_value == 'No':
+            return False
+        else:
+            raise Exception("found incompatible agreement", raw_value)
